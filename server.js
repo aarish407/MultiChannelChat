73cfb110982644411b0f3bcc4caf618b5592ca34
aarish407 <aarish407@gmail.com>
2018-03-08 15:54:19 +0530

    Added basic client server connection4

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
+
+// app.use(express.static('./public'))
+
+app.get('/', function (req, res) {
+    // res.sendFile('./index.html');
+    res.sendFile('index.html', { root: __dirname });
+});
+
+server.listen(process.env.PORT || 3009, '0.0.0.0', function () {
+    console.log('Listening on ' + server.address().port);
+});
+
+io.on('connection', function (socket) {
+    console.log("Socket connection established");
+
+    socket.on('clientHasJoined', function () {
+       console.log("Client has joined with id= "+socket.id); 
+    });
+});
